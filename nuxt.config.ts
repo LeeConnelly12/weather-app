54de0853c76059746144c928fdc09f1dd99d28be
Lee Connelly <lee@leeconnelly.dev>
2025-09-05 20:29:09 +0100

    Add fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  css: ['~/assets/css/app.css'],
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
-   tailwindcss: {
+  tailwindcss: {
     exposeConfig: true,
     viewer: true,
-  }
+  },
 })
