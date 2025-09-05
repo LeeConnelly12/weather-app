761b4d8d65434b8a3f12a84dd8750af46d809c48
Lee Connelly <lee@leeconnelly.dev>
2025-09-05 23:11:13 +0100

    Fetch forecast for places

--- app/app.vue
+++ app/app.vue
@@ -10,10 +10,39 @@ const search = useState('search', () => '')
 
 const results = useState('results', () => [])
 
+const place = useState('place', () => null)
+
 const submit = async () => {
   const response = await fetchCoordsForLocation(search.value)
 
   results.value = response.results
+}
+
+const dailyForecast = computed(() => {
+  if (!place.value?.forecast?.time) {
+    return []
+  }
+
+  const forecast = place.value.forecast
+
+  return forecast.time.map((date) => {
+    const index = forecast.time.indexOf(date)
+
+    return {
+      day: 'Tue',
+      min: forecast.temperature_2m_min[index],
+      max: forecast.temperature_2m_max[index],
+    }
+  })
+})
+
+const setPlace = async (result) => {
+  const forecast = await fetchForecastForCoords(result.latitude, result.longitude)
+
+  place.value = {
+    name: result.name,
+    forecast: forecast,
+  }
 }
 </script>
 
@@ -26,10 +55,24 @@ const submit = async () => {
     </form>
     <ul v-if="results.length">
       <li v-for="result in results" :key="result.id">
-        <button type="button">
+        <button type="button" @click="setPlace(result)">
           {{ result.name }}
         </button>
       </li>
     </ul>
+    <div v-if="place">
+      <h2>{{ place.name }}</h2>
+      <p>{{ new Date().toLocaleDateString() }}</p>
+    </div>
+    <div v-if="dailyForecast.length">
+      <h2>Daily forecast</h2>
+      <ul>
+        <li v-for="forecast in dailyForecast" :key="forecast.day">
+          <p>{{ forecast.day }}</p>
+          <p>{{ forecast.min }}</p>
+          <p>{{ forecast.max }}</p>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
--- app/utils/meteo.ts
+++ app/utils/meteo.ts
@@ -1,3 +1,5 @@
+import { fetchWeatherApi } from 'openmeteo'
+
 export async function fetchCoordsForLocation(location: string) {
   const url = 'https://geocoding-api.open-meteo.com/v1/search'
 
@@ -8,4 +10,39 @@ export async function fetchCoordsForLocation(location: string) {
   const { data } = await useFetch(url + '?' + params.toString())
 
   return data.value
+}
+
+export async function fetchForecastForCoords(latitude: number, longitude: number) {
+  const url = 'https://api.open-meteo.com/v1/forecast'
+
+  const params = {
+    latitude: latitude,
+    longitude: longitude,
+    daily: 'temperature_2m_min,temperature_2m_max',
+  }
+
+  const responses = await fetchWeatherApi(url, params)
+
+  const response = responses[0]
+
+  const utcOffsetSeconds = response.utcOffsetSeconds()
+  const daily = response.daily?.() || null
+
+  const dailyTimeArray = daily
+    ? [...Array((Number(daily.timeEnd()) - Number(daily.time())) / daily.interval())].map((_, i) =>
+        new Date((Number(daily.time()) + i * daily.interval() + utcOffsetSeconds) * 1000).toISOString().slice(0, 10),
+      )
+    : []
+
+  const weatherData = {
+    ...(daily && {
+      daily: {
+        time: dailyTimeArray,
+        temperature_2m_min: daily.variables(0)!.valuesArray(),
+        temperature_2m_max: daily.variables(1)!.valuesArray(),
+      },
+    }),
+  }
+
+  return weatherData.daily
 }
