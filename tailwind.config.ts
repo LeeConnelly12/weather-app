38b78778ca71a4aa18e86e138af5a604344fee32
Lee Connelly <lee@leeconnelly.dev>
2025-09-05 20:48:29 +0100

    Add colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,23 @@ export default <Partial<Config>>{
       fontFamily: {
         sans: ['DM Sans', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        neutral: {
+          900: '#02012C',
+          800: '#262540',
+          700: '#302F4A',
+          600: '#3C3B5E',
+          300: '#ACACB7',
+          200: '#D4D3D9',
+        },
+        orange: {
+          500: '#FF820A',
+        },
+        blue: {
+          500: '#4658D9',
+          700: '#2B1B9C',
+        },
+      },
     },
   },
 }
