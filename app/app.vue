1718b622facedd1142790a9adeff9d75c8cefea9
Lee Connelly <lee@leeconnelly.dev>
2025-09-06 15:38:20 +0100

    Inline SVG icons

--- app/app.vue
+++ app/app.vue
@@ -111,26 +111,28 @@ const switchUnits = async () => {
 
 <template>
   <div>
-    <nav class="flex items-center justify-between lg:mx-auto lg:max-w-7xl">
+    <nav class="flex items-center justify-between xl:mx-auto xl:max-w-7xl">
       <Logo class="w-[138px] md:w-[197px]" />
       <button
         type="button"
         class="flex items-center gap-[0.375rem] rounded-md bg-neutral-800 px-2.5 py-2 outline-none focus:outline-2 focus:outline-white md:gap-2"
       >
-        <img src="/images/icon-units.svg" alt="" width="16" height="16" />
+        <Units />
         <p class="text-sm">Units</p>
-        <img src="/images/icon-dropdown.svg" alt="" width="13" height="8" />
+        <Chevron />
       </button>
     </nav>
-    <div class="mt-12 lg:mt-16">
-      <h1 class="text-center font-bricolage-grotesque text-6xl font-bold md:mx-auto md:max-w-lg lg:max-w-none">How's the sky looking today?</h1>
-      <form @submit.prevent="submit" class="mt-12 items-center md:grid md:grid-cols-[1fr_auto] md:gap-4 lg:mx-auto lg:mt-16 lg:max-w-2xl">
+    <div class="mt-12 xl:mt-16">
+      <h1 class="text-center font-bricolage-grotesque text-6xl font-bold md:mx-auto md:max-w-lg xl:max-w-none">How's the sky looking today?</h1>
+      <form @submit.prevent="submit" class="mt-12 items-center md:grid md:grid-cols-[1fr_auto] md:gap-4 xl:mx-auto xl:mt-16 xl:max-w-2xl">
         <div class="relative flex items-center">
-          <img class="pointer-events-none absolute left-6" src="/images/icon-search.svg" alt="" />
+          <Search class="pointer-events-none absolute left-6" />
           <input
             v-model="search"
-            type="text"
+            type="search"
             placeholder="Search for a place..."
+            required
+            minlength="3"
             class="h-14 w-full rounded-xl bg-neutral-800 pl-[3.75rem] pr-6 text-white outline-none placeholder:text-neutral-200 focus:outline-2 focus:outline-white"
           />
           <ul
@@ -161,8 +163,8 @@ const switchUnits = async () => {
         </button>
       </form>
     </div>
-    <div v-if="place" class="lg:mx-auto lg:mt-12 lg:grid lg:max-w-7xl lg:grid-cols-[50rem_24rem] lg:gap-8">
-      <div class="mt-8 lg:mt-0 lg:grid lg:grid-rows-[auto_auto_1fr_auto]">
+    <div v-if="place" class="xl:mx-auto xl:mt-12 xl:grid xl:max-w-7xl xl:grid-cols-[50rem_24rem] xl:gap-8">
+      <div class="mt-8 xl:mt-0 xl:grid xl:grid-rows-[auto_auto_1fr_auto]">
         <div
           class="h-[286px] w-full rounded-[1.25rem] bg-today-small bg-cover bg-no-repeat py-10 text-center md:flex md:items-center md:justify-between md:bg-today-large md:px-6 md:text-left"
         >
@@ -175,7 +177,7 @@ const switchUnits = async () => {
             <p class="text-8xl">{{ place.temperature }}</p>
           </div>
         </div>
-        <div class="mt-5 grid grid-cols-2 gap-4 md:grid-cols-4 lg:mt-8 lg:gap-6">
+        <div class="mt-5 grid grid-cols-2 gap-4 md:grid-cols-4 xl:mt-8 xl:gap-6">
           <div class="rounded-xl border border-neutral-600 bg-neutral-800 p-5">
             <p class="text-lg text-neutral-200">Feels Like</p>
             <p class="mt-6 text-4xl font-light">{{ place.feelsLike }}</p>
@@ -193,7 +195,7 @@ const switchUnits = async () => {
             <p class="mt-6 text-4xl font-light">{{ place.precipitation }}</p>
           </div>
         </div>
-        <div v-if="dailyForecast.length" class="mt-8 lg:row-start-4 lg:mt-0">
+        <div v-if="dailyForecast.length" class="mt-8 xl:row-start-4 xl:mt-0">
           <h2 class="text-xl font-semibold">Daily forecast</h2>
           <ul class="mt-5 grid grid-cols-[repeat(auto-fit,minmax(89px,1fr))] gap-4">
             <li
@@ -211,19 +213,22 @@ const switchUnits = async () => {
           </ul>
         </div>
       </div>
-      <div v-if="hourlyForecast.length" class="mt-8 rounded-xl bg-neutral-800 px-4 py-5 md:p-6 lg:mt-0">
+      <div v-if="hourlyForecast.length" class="mt-8 rounded-xl bg-neutral-800 px-4 py-5 md:p-6 xl:mt-0">
         <div class="flex items-center justify-between">
           <h2 class="text-xl font-semibold">Hourly forecast</h2>
-          <button type="button" class="flex gap-[0.375rem] rounded-md bg-neutral-600 px-4 py-2 outline-none focus:outline-2 focus:outline-white">
+          <button
+            type="button"
+            class="flex items-center gap-[0.375rem] rounded-md bg-neutral-600 px-4 py-2 outline-none focus:outline-2 focus:outline-white"
+          >
             <p>{{ selectedDay }}</p>
-            <img src="/images/icon-dropdown.svg" alt="" width="13" height="8" />
+            <Chevron />
           </button>
         </div>
-        <ul>
+        <ul class="mt-4 space-y-4">
           <li
             v-for="forecast in hourlyForecast"
             :key="forecast.time"
-            class="mt-4 grid grid-cols-[auto_auto_1fr_auto] items-center gap-2 rounded-lg border border-neutral-600 bg-neutral-700 py-1.5 pl-3 pr-4"
+            class="grid grid-cols-[auto_auto_1fr_auto] items-center gap-2 rounded-lg border border-neutral-600 bg-neutral-700 py-1.5 pl-3 pr-4"
           >
             <WeatherCode :weatherCode="forecast.weatherCode" class="size-[40px]" />
             <p class="text-lg">{{ forecast.time }}</p>
