7b78b58815abf02453c58df7c748897d62ac1025
Lee Connelly <lee@leeconnelly.dev>
2025-09-26 20:53:02 +0100

    Increase padding

--- app/app.vue
+++ app/app.vue
@@ -84,11 +84,11 @@ onMounted(async () => {
     <SearchForm @selectedResult="setPlace" />
     <section v-if="preferences.viewedLocations.length" class="mt-8 xl:mx-auto xl:max-w-7xl">
       <h2 class="text-xl font-semibold">Viewed places</h2>
-      <ul class="mt-5 grid gap-4 sm:grid-cols-2 md:grid-cols-4">
+      <ul class="mt-5 grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
         <li v-for="location in preferences.viewedLocations" class="flex">
           <button
             @click="setPlace(location)"
-            class="flex w-full flex-col rounded-xl border border-neutral-600 bg-neutral-800 px-2.5 py-4 text-left outline-none focus:outline-2 focus:outline-white"
+            class="flex w-full flex-col rounded-xl border border-neutral-600 bg-neutral-800 p-5 text-left outline-none focus:outline-2 focus:outline-white"
             :aria-label="`View forecast for ${location.name}`"
           >
             <div class="text-lg">{{ location.name }}</div>
