8ff58867ef3e2b1ead7e906ef23aad4da967b042
Lee Connelly <lee@leeconnelly.dev>
2025-09-26 20:18:30 +0100

    Don't request same location

--- app/app.vue
+++ app/app.vue
@@ -10,6 +10,10 @@ const dailyData = ref(null)
 const hourlyData = ref(null)
 
 const setPlace = async (result) => {
+  if (result === activeResult.value) {
+    return
+  }
+
   activeResult.value = result
 
   loading.value = true
